1eea30b417f65856ff4ecc69f715033291cf95d2
rubeushagrid66 <naimyzamzam@gmail.com>
2026-03-05 02:31:52 +0700

    Create Footer.jsx

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="bg-white border-t border-slate-200 py-6 mt-12 text-center">
+      <p className="text-xs text-slate-500 font-medium">
+        &copy; 2026. Made by Naimy.
+      </p>
+    </footer>
+  );
+}
